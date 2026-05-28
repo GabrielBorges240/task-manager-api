c0fecf9d4a3c084c4d6ad4a9d0f17f37e00b01d5
Gabriel Borges <gabrieuborge@gmail.com>
2026-05-28 15:35:01 -0400

    Add app/schemas/__init__.py

---
+++ app/schemas/__init__.py
@@ -0,0 +1,2 @@
+from app.schemas.usuario import UsuarioCriar, UsuarioAtualizar, UsuarioResposta, LoginSchema, TokenResposta
+from app.schemas.tarefa import TarefaCriar, TarefaAtualizar, TarefaResposta, TarefaListagem
